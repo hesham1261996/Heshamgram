9d65fc2271ba94048d8ef70e6b01edb81ed902f0
unknown <hesham1261996@gmial.com>
2023-03-23 11:33:05 +0200

    relationships between models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Models;
+use App\Models\Post;
+use App\Models\User;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +10,12 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post(){
+        return $this->belongsTo(Post::class);
+    }
+
+    public function owner(){
+        return $this->belongsTo(User::class , "user_id");
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User ;
+use App\Models\Comment ;
 
 class Post extends Model
 {
     use HasFactory;
+    public function owner(){
+        return $this->belongsTo(User::class , "user_id");
+    }
+
+    public function comments(){
+        return  $this->hasMany(Comment::class);
+    }
 }
